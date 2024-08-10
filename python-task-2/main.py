50574c35d0a34eb8ba68606338c926aa58499191
Yaroslav Tsalko <yaroslav_tsalko@epam.com>
2024-08-10 18:21:24 +0300

    Updated cli with some base arguments and added subarguments

--- python-task-2/main.py
+++ python-task-2/main.py
@@ -1,1 +1,66 @@
-print('Hello World!')
+import sys
+import logging
+import argparse
+
+parser = argparse.ArgumentParser(description='Database Backup and Restore utility')
+subparsers = parser.add_subparsers(dest='command', help='commands')
+
+backup_parser = subparsers.add_parser('backup', help='Backup database')
+
+backup_parser.add_argument(
+    '--db',
+    required=True,
+    help='Database name',
+    action='store'
+)
+backup_parser.add_argument(
+    '--table',
+    help='Table to backup.',
+    action='store'
+)
+backup_parser.add_argument(
+    '--type',
+    choices=['structure', 'data', 'full'],
+    help='Type of database backup'
+)
+
+backup_parser.add_argument(
+    "--save-one",
+    help="Save all in one",
+    action="store_true"
+)
+backup_parser.add_argument(
+    "--save-multi",
+    help="Save in multiple files.",
+    action="store_true"
+)
+backup_parser.add_argument(
+    "--save-into",
+    help="Specify in which folder save backup.",
+    required=True
+)
+
+restore_parser = subparsers.add_parser('restore', help='Restore database')
+restore_parser.add_argument(
+    '--file',
+    help='File to restore.',
+    required=True
+)
+restore_parser.add_argument(
+    '--type',
+    choices=['structure', 'data', 'full'],
+    help='Type of database restoration.',
+    action='store'
+)
+restore_parser.add_argument(
+    '--table',
+    help='Table to restore.',
+    action='store'
+)
+parser.add_argument(
+    '-v', '--verbose',
+    action='count',
+    help='verbose mode'
+)
+
+args = parser.parse_args()
