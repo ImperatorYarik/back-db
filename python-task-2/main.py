67f6bc0973b59651711d43b928144f78d7d69030
Yaroslav Tsalko <yaroslav_tsalko@epam.com>
2024-08-15 15:03:39 +0300

    Save commit

--- python-task-2/main.py
+++ python-task-2/main.py
@@ -1,73 +1,94 @@
-import os
 import logging
 import argparse
+from email.policy import default
 
-from dotenv import load_dotenv
+import src.backup as backup
 
 
-load_dotenv()
+def backup_controller(args):
+    bk = backup.Backup(db_type=args.db_type, database_name=args.db, connection_string=args.connection_string,
+                       table_name=args.table, type=args.type, is_save_one=args.save_one,
+                       is_save_multiple=args.save_multi, save_into=args.save_into)
+    return bk.backup_data()
 
-parser = argparse.ArgumentParser(description='Database Backup and Restore utility')
-subparsers = parser.add_subparsers(dest='command', help='commands')
-backup_parser = subparsers.add_parser('backup', help='Backup database')
 
-os.makedirs('backups', exist_ok=True)
+def main():
+    parser = argparse.ArgumentParser(description='Database Backup and Restore utility')
+    subparsers = parser.add_subparsers(dest='command', help='commands')
+    backup_parser = subparsers.add_parser('backup', help='Backup database')
 
-backup_parser.add_argument(
-    '--db',
-    required=True,
-    help='Database name',
-    action='store'
-)
-backup_parser.add_argument(
-    '--table',
-    help='Table to backup.',
-    action='store'
-)
-backup_parser.add_argument(
-    '--type',
-    choices=['structure', 'data'],
-    default='full',
-    help='Type of database backup'
-)
+    backup_parser.add_argument(
+        '--db',
+        required=True,
+        help='Database name',
+        action='store'
+    )
+    backup_parser.add_argument(
+        '--db-type',
+        required=True,
+        action='store'
+    )
+    backup_parser.add_argument(
+        '--connection-string',
+        required=True,
+        action='store'
+    )
+    backup_parser.add_argument(
+        '--table',
+        help='Table to backup.',
+        action='store',
+        default=None
+    )
+    backup_parser.add_argument(
+        '--type',
+        choices=['structure', 'data'],
+        default=None,
+        help='Type of database backup'
+    )
 
-backup_parser.add_argument(
-    "--save-one",
-    help="Save all in one",
-    action="store_true"
-)
-backup_parser.add_argument(
-    "--save-multi",
-    help="Save in multiple files.",
-    action="store_true"
-)
-backup_parser.add_argument(
-    "--save-into",
-    help="Specify in which folder save backup.",
-)
+    backup_parser.add_argument(
+        "--save-one",
+        help="Save all in one",
+        action="store_true"
+    )
+    backup_parser.add_argument(
+        "--save-multi",
+        help="Save in multiple files.",
+        action="store_true"
+    )
+    backup_parser.add_argument(
+        "--save-into",
+        help="Specify in which folder save backup.",
+    )
 
-restore_parser = subparsers.add_parser('restore', help='Restore database')
-restore_parser.add_argument(
-    '--file',
-    help='File to restore.',
-    required=True
-)
-restore_parser.add_argument(
-    '--type',
-    choices=['structure', 'data', 'full'],
-    help='Type of database restoration.',
-    action='store'
-)
-restore_parser.add_argument(
-    '--table',
-    help='Table to restore.',
-    action='store'
-)
-parser.add_argument(
-    '-v', '--verbose',
-    action='count',
-    help='verbose mode'
-)
+    restore_parser = subparsers.add_parser('restore', help='Restore database')
+    restore_parser.add_argument(
+        '--file',
+        help='File to restore.',
+        required=True
+    )
+    restore_parser.add_argument(
+        '--type',
+        choices=['structure', 'data', 'full'],
+        help='Type of database restoration.',
+        action='store'
+    )
+    restore_parser.add_argument(
+        '--table',
+        help='Table to restore.',
+        action='store'
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        action='count',
+        help='verbose mode'
+    )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
+    if args.command == 'backup':
+        print(backup_controller(args))
+
+
+if __name__ == '__main__':
+    main()
--- python-task-2/src/backup.py
+++ python-task-2/src/backup.py
@@ -1,11 +1,17 @@
 import os
+from datetime import datetime
 import logging
+
+from src.models import dbmysql as mysql
+
+
 class Backup:
 
-    def __init__(self, database_name: str, connection_string: str, table_name: str = None, type: str = None,
+    def __init__(self, db_type: str, database_name: str, connection_string: str, table_name: str = None,
+                 type: str = None,
                  is_save_one: bool = False, is_save_multiple: bool = False, save_into: str = None) -> None:
         logger = logging.getLogger(__name__)
-
+        self.db_type = db_type
         self.database_name = database_name
         self.connection_string = connection_string
         self.table_name = table_name
@@ -13,10 +19,33 @@ class Backup:
         self.is_save_one = is_save_one
         self.is_save_multiple = is_save_multiple
         self.save_into = save_into
-        if save_into is not None:
+        if self.save_into is not None:
             if os.path.isdir(save_into):
                 os.makedirs(save_into, exist_ok=True)
             else:
                 logger.error('Provided path is not dir or does\'t exist!')
+        else:
+            self.save_into = 'backup'
+            os.makedirs(self.save_into, exist_ok=True)
 
+    def backup_database(self) -> None:
+        """Delegates backup tasks to another methods"""
+        match self.db_type:
+            case 'structure':
+                now = datetime.now()
+                timestamp = now.timestamp()
+                with open(f'{self.save_into}/{timestamp}-structure.sql', 'w') as f:
+                    f.write(self.backup_structure())
+            case 'data':
+                pass
+            case _:
+                result = f'{self.backup_structure()}\n\n\n' + f'{self.backup_data()}'
+                return result
 
+    def backup_structure(self):
+        if self.db_type == 'mysql':
+            db = mysql.MySQL(connection_string=self.connection_string, database_name=self.database_name)
+            return db.get_database_structure()
+
+    def backup_data(self):
+        pass
