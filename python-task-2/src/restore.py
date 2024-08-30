a3a053700c5a88123fbac556b4f1a3cacda56285
Yaroslav Tsalko <yaroslav_tsalko@epam.com>
2024-08-30 23:42:36 +0300

    Refactored restore, WIP

--- python-task-2/src/restore.py
+++ python-task-2/src/restore.py
@@ -18,43 +18,49 @@ class Restore:
         self.connection_string = connection_string
         self.backup_version = backup_version
         if self.backup_version is None:
-            all_versions = []
             filenames = os.listdir(self.file)
-            for filename in filenames:
-                temp = filename.split('-')
-                print(temp)
-                if temp[1] == f'{self.restore_type}.sql':
-                    all_versions.append(temp[0])
-                    self.backup_version = max(all_versions)
+            self.backup_version = max(filenames)
 
-    def restore_structure(self, database_structure):
+    def restore_sql(self, sql: str) -> str:
         if self.db_type == 'mysql':
             db = mysql.MySQL(connection_string=self.connection_string, database_name=self.database_name)
-            return db.restore_database_structure(database_structure=database_structure)
+            return db.restore_database_sql(sql=sql)
 
-    def restore_data(self, database_data):
-        if self.db_type == 'mysql':
-            db = mysql.MySQL(connection_string=self.connection_string, database_name=self.database_name)
-            return db.restore_database_data(database_data=database_data)
+
 
     def restore_database(self):
         """
         Delegates restoration tasks to another methods
         """
-        match self.restore_type:
-            case 'structure':
-                with open(f'{self.file}/{self.backup_version}-structure.sql', 'r') as file:
-                    structure = file.read()
-                return self.restore_structure(structure)
+        # TODO: Make selection menu for database backups
+        # TODO: Add 'SELECT ALL' feature
+        version_folder = f'{self.file}/{self.backup_version}'
+        files = os.listdir(version_folder)
+        if len(files) > 1:
+            file_number = 0
+            files_list = []
+            sql = ''
+            for file in files:
+                files_list.append(file)
+                print(f'[{file_number}] {file}')
+                file_number += 1
+            isok = False
+            choice_list = []
+            while not isok:
+                choice = input("Please select a file to restore (if you want to restore multiple files, make spaces between numbers): ")
+                choice_list = choice.strip().split(' ')
+                for number in choice:
+                    if int(number) and int(number) < len(files_list) or number == '*':
+                        isok = True
+                    else:
+                        print('Please insert correct number!')
+            print(choice_list)
+            for number in choice_list:
+                sql += files_list[int(number)]
+                print(sql)
 
-            case 'data':
-                with open(f'{self.file}/{self.backup_version}-data.sql', 'r') as file:
-                    data = file.read()
-                return self.restore_data(data)
-            case _:
-                with open(f'{self.file}/{self.backup_version}-full.sql', 'w') as file:
-                    data = file.read()
-
-                elements = data.split('-- DATA --')
-                return self.restore_structure(elements[0]), self.restore_data(elements[1])
+        else:
+            with open(files[0], 'r') as f:
+                sql = f.read()
+            return self.restore_sql(sql=sql)
 
