a9878ccf91b4bc60f775358e70af1155a488293a
Yaroslav Tsalko <yaroslav_tsalko@epam.com>
2024-09-03 19:25:44 +0300

    Added types for restoration, added table restoration

--- python-task-2/src/restore.py
+++ python-task-2/src/restore.py
@@ -27,22 +27,63 @@ class Restore:
             db = mysql.MySQL(connection_string=self.connection_string, database_name=self.database_name)
             return db.restore_database_sql(sql=sql)
 
-
-
     def restore_database(self):
         """
         Delegates restoration tasks to another methods
         """
         version_folder = f'{self.file}/{self.backup_version}'
-        files = os.listdir(version_folder)
-        if self.restore_type:
-            pass
+        files = []
+        if self.table_name:
+            for file in os.listdir(version_folder):
+                if self.table_name in file:
+                    files.append(self.table_name)
         else:
-            sql_types = {
-                "ddl": [],
-                "dml": [],
-                "dcl": []
-            }
+            files = os.listdir(version_folder)
+
+        sql_types = {
+            "ddl": [],
+            "dml": [],
+            "dcl": []
+        }
+
+        if self.restore_type:
+            match self.restore_type:
+                case 'structure':
+                    for file in files:
+                        if "DDL" in file:
+                            sql_types["ddl"].append(file)
+                        elif "DML" in file:
+                            continue
+                        elif "DCL" in file:
+                            continue
+                        else:
+                            with open(version_folder + '/' + file, 'r') as f:
+                                sql = f.read()
+                            self.restore_sql(sql=sql)
+                        for _ in range(2):
+                            for sql_file in sql_types["ddl"]:
+                                with open(version_folder + '/' + sql_file, 'r') as f:
+                                    sql = f.read()
+                                self.restore_sql(sql=sql)
+                case 'data':
+                    for file in files:
+                        if "DDL" in file:
+                            continue
+                        elif "DML" in file:
+                            sql_types["dml"].append(file)
+                        elif "DCL" in file:
+                            continue
+                        else:
+                            with open(version_folder + '/' + file, 'r') as f:
+                                sql = f.read()
+                            self.restore_sql(sql=sql)
+                        for _ in range(2):
+                            for sql_file in sql_types["dml"]:
+                                with open(version_folder + '/' + sql_file, 'r') as f:
+                                    sql = f.read()
+                                self.restore_sql(sql=sql)
+
+        else:
             for file in files:
                 if "DDL" in file:
                     sql_types["ddl"].append(file)
@@ -54,19 +95,21 @@ class Restore:
                     with open(version_folder + '/' + file, 'r') as f:
                         sql = f.read()
                     self.restore_sql(sql=sql)
+
             for _ in range(2):
                 for sql_file in sql_types["ddl"]:
                     with open(version_folder + '/' + sql_file, 'r') as f:
                         sql = f.read()
                     self.restore_sql(sql=sql)
+
             for sql_file in sql_types["dml"]:
                 with open(version_folder + '/' + sql_file, 'r') as f:
                     sql = f.read()
                 self.restore_sql(sql=sql)
+
             for sql_file in sql_types["dcl"]:
                 with open(version_folder + '/' + sql_file, 'r') as f:
                     sql = f.read()
                 self.restore_sql(sql=sql)
+
         return 'Successfully restored!'
-
-
