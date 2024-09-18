74f2b8b8ad2ebe10fdf1332f9f61812e801c05b4
Yaroslav Tsalko <yaroslav_tsalko@epam.com>
2024-09-18 17:55:55 +0300

    Updated commands.sh

--- python-task-2/commands.sh
+++ python-task-2/commands.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
-#python3 main.py backup --db sakila --db-type mysql --connection-string mysql://root:root@localhost:3306/
 
-#python3 main.py backup --db sakila --db-type mysql --connection-string mysql://root:root@localhost:3306/ --save-multi
+## MSQL ##
+python3 main.py backup --db sakila --db-type mysql --connection-string mysql://root:root@localhost:3306/ --save-multi
+sleep 1
+python3 main.py backup --db sakila --db-type mysql --connection-string mysql://root:root@localhost:3306/ --type structure
+sleep 1
+python3 main.py backup --db sakila --db-type mysql --connection-string mysql://root:root@localhost:3306/ --type data
+sleep 1
+python3 main.py backup --db sakila --db-type mysql --connection-string mysql://root:root@localhost:3306/
+sleep 1
 
-#python3 main.py restore --db sakila --db-type mysql --connection-string mysql://root:root@localhost:3306/ --type structure
+## POSTGRESQL ##
+
+python3 main.py backup --db northwind --db-type postgresql --connection-string postgresql://dumper:dumper@localhost:5432/ --save-multi
+sleep 1
+python3 main.py backup --db northwind --db-type postgresql --connection-string postgresql://dumper:dumper@localhost:5432/ --type structure
+sleep 1
+python3 main.py backup --db northwind --db-type postgresql --connection-string postgresql://dumper:dumper@localhost:5432/ --type data
+sleep 1
+python3 main.py backup --db northwind --db-type postgresql --connection-string postgresql://dumper:dumper@localhost:5432/
+sleep 1
+
+
+## RESTORE ##
 
 #python3 main.py restore --db sakila --db-type mysql --connection-string mysql://root:root@localhost:3307/ --backup-version 1725030638
-
-#python3 main.py -vvv restore --db sakila --db-type mysql --connection-string mysql://root:root@localhost:3307/
-
-#python3 main.py backup --db sakila --db-type mysql --connection-string mysql://root:root@localhost:3306/ --save-multi
-
-#python3 main.py backup --db northwind --db-type postgresql --connection-string postgresql://dumper:dumper@localhost:5432/ --save-multi
 
 #python3 main.py restore --db northwind --db-type postgresql --connection-string postgresql://dumper:dumper@localhost:5433/
 
